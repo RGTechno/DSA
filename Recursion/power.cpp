c1c3802d8245f433f78111f6fcc3ba7e376a9b3a
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-09-13 17:39:01 +0530

    power

---
+++ Recursion/power.cpp
@@ -0,0 +1,22 @@
+// find a^b;
+
+#include <iostream>
+using namespace std;
+
+int power(int a,int b){
+
+    if(b==0) return 1;
+
+    return a*power(a,b-1);
+}
+
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+
+    cout<<power(a,b);
+
+    return 0;
+}
