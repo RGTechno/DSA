d360ad987dc038d6be6a9a778ba6c580ef4e3278
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-09-13 17:49:04 +0530

    power optimised

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -3,20 +3,40 @@
 #include <iostream>
 using namespace std;
 
-int power(int a,int b){
+int power(int a, int b)
+{
 
-    if(b==0) return 1;
+    if (b == 0)
+        return 1;
 
-    return a*power(a,b-1);
+    return a * power(a, b - 1);
+}
+
+int power_optimised(int a, int b)
+{
+    if (b == 0)
+        return 1;
+
+    int a1 = power_optimised(a, b / 2);
+
+    int ans = a1 * a1;
+
+    if (b & 1)
+    {
+        return a * ans;
+    }
+
+    return ans;
 }
 
 int main()
 {
 
-    int a,b;
-    cin>>a>>b;
+    int a, b;
+    cin >> a >> b;
 
-    cout<<power(a,b);
+    cout << power(a, b) << endl;
+    cout << power_optimised(a, b);
 
     return 0;
 }
