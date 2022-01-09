b43c101113c44c1188a604ba60e0e56f85af84fb
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2022-01-09 07:59:49 +0530

    .

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -2,35 +2,35 @@
 #include <vector>
 using namespace std;
 
-/* O(2^n)
+/* O(2^n) */
+vector<int> dp(100,-1);
 int fib(int n)
 {
 
     if (n == 0 || n == 1)
         return n;
 
-    return fib(n - 1) + fib(n - 2);
+    return dp[n] = fib(n - 1) + fib(n - 2);
 }
-*/
+
 
 
 // O(n)
 
 // Bottom Up DP => going from smaller subproblem to larger problem by memoisation
-int fib(int n)
-{
-    vector<int> dp(n + 1);
+// int fib(int n)
+// {
 
-    dp[0] = 0;
-    dp[1] = 1;
+//     dp[0] = 0;
+//     dp[1] = 1;
 
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
+//     for (int i = 2; i <= n; i++)
+//     {
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
 
-    return dp[n];
-}
+//     return dp[n];
+// }
 
 int main()
 {
